96c7f90f8eac9fc5ebcadec9ecfb469e52e10b18
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2024-04-24 19:52:12 +0300

    SVG logo fixes

--- src/MolliePaymentGateway.php
+++ src/MolliePaymentGateway.php
@@ -34,7 +34,7 @@ class MolliePaymentGateway implements PaymentGateway
 
     public static function svgIcon(): string
     {
-        return self::$svg ??= file_get_contents(__DIR__ . '/../../resources/logo.svg');
+        return self::$svg ??= file_get_contents(__DIR__ . '/resources/logo.svg');
     }
 
     public function createPaymentRequest(Payment $payment, Address $shippingAddress = null, array $options = []): PaymentRequest
